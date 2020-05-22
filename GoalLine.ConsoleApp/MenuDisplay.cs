3f6a2b96834c964cb353d2d0fa3f242c98965ada
Paul Turner <paul.turner611@gmail.com>
2020-05-22 23:17:58 +0100

    Fixed bug in menu system where the game moves to the current day no matter which menu option was selected

--- GoalLine.ConsoleApp/MenuDisplay.cs
+++ GoalLine.ConsoleApp/MenuDisplay.cs
@@ -13,66 +13,74 @@ namespace GoalLine.ConsoleApp
     {
         public void Display()
         {
-            StandardUI gui = new StandardUI();
-
-            WorldAdapter wa = new WorldAdapter();
-
-            TeamAdapter ta = new TeamAdapter();
-            Team CurrentTeam = ta.GetTeamByManager(wa.CurrentManagerID);
-
-            ManagerAdapter ma = new ManagerAdapter();
-            Manager CurrentManager = ma.GetManager(wa.CurrentManagerID);
-
-            FixtureAdapter fa = new FixtureAdapter();
-
-            gui.BarText = CurrentManager.Name + " - " +
-                          CurrentTeam.Name;
-            gui.SetupScreen();
-
-            Menu mnu = new Menu();
-            mnu.AddColumn(new MenuColumn("Select an Option", ConsoleColor.White, 40));
-
-            if(fa.IsTodayAMatchDay())
+            bool EndOfDay = false;
+            while (!EndOfDay)
             {
-                mnu.AddItem(new MenuItem("PLAY", new string[] { "******** Play Matches ********" }));
-            } else
-            {
-                mnu.AddItem(new MenuItem("NOWT", new string[] { "Do Nothing Today" }));
+                StandardUI gui = new StandardUI();
+
+                WorldAdapter wa = new WorldAdapter();
+
+                TeamAdapter ta = new TeamAdapter();
+                Team CurrentTeam = ta.GetTeamByManager(wa.CurrentManagerID);
+
+                ManagerAdapter ma = new ManagerAdapter();
+                Manager CurrentManager = ma.GetManager(wa.CurrentManagerID);
+
+                FixtureAdapter fa = new FixtureAdapter();
+
+                gui.BarText = CurrentManager.Name + " - " +
+                              CurrentTeam.Name;
+                gui.SetupScreen();
+
+                Menu mnu = new Menu();
+                mnu.AddColumn(new MenuColumn("Select an Option", ConsoleColor.White, 40));
+
+                if (fa.IsTodayAMatchDay())
+                {
+                    mnu.AddItem(new MenuItem("PLAY", new string[] { "******** Play Matches ********" }));
+                }
+                else
+                {
+                    mnu.AddItem(new MenuItem("NOWT", new string[] { "Do Nothing Today" }));
+                }
+
+                mnu.AddItem(new MenuItem("TEAM", new string[] { "View My Team" }));
+                mnu.AddItem(new MenuItem("FIX", new string[] { "View League Fixtures" }));
+
+
+
+                mnu.AddItem(new MenuItem("QUIT", new string[] { "Quit Game" }));
+
+                MenuReturnData menuRet = mnu.RunMenu();
+                switch (menuRet.ItemID)
+                {
+                    case "NOWT":
+                        EndOfDay = true;
+                        break;
+
+                    case "TEAM":
+                        TeamDisplay td = new TeamDisplay();
+                        td.TeamID = CurrentManager.CurrentTeam;
+                        td.Display();
+                        break;
+
+                    case "FIX":
+                        FixtureDisplay fd = new FixtureDisplay();
+                        fd.Display(CurrentTeam);
+                        break;
+
+                    case "PLAY":
+                        Matchday md = new Matchday();
+                        md.Run();
+                        EndOfDay = true;
+                        break;
+
+                    case "QUIT":
+                        Environment.Exit(0);
+                        break;
+                }
             }
-
-            mnu.AddItem(new MenuItem("TEAM", new string[] { "View My Team" }));
-            mnu.AddItem(new MenuItem("FIX", new string[] { "View League Fixtures" }));
             
-
-
-            mnu.AddItem(new MenuItem("QUIT", new string[] { "Quit Game" }));
-
-            MenuReturnData menuRet = mnu.RunMenu();
-            switch(menuRet.ItemID)
-            {
-                case "NOWT":
-                    break;
-
-                case "TEAM":
-                    TeamDisplay td = new TeamDisplay();
-                    td.TeamID = CurrentManager.CurrentTeam;
-                    td.Display();
-                    break;
-
-                case "FIX":
-                    FixtureDisplay fd = new FixtureDisplay();
-                    fd.Display(CurrentTeam);
-                    break;
-
-                case "PLAY":
-                    Matchday md = new Matchday();
-                    md.Run();
-                    break;
-
-                case "QUIT":
-                    Environment.Exit(0);
-                    break;
-            }
         }
         
     }
