6c7d594655fb5f9d5438f3c25fc48ede7753f629
Paul Turner <paul.turner611@gmail.com>
2020-06-29 23:16:37 +0100

    Starting work on a WPF/XAML-based UI for the game

---
+++ GoalLine.UI/GameControls/StartControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GoalLine.Data;
+using GoalLine.Structures;
+using GoalLine.UI.SingleControls;
+
+namespace GoalLine.UI
+{
+    /// <summary>
+    /// Interaction logic for StartControl.xaml
+    /// </summary>
+    public partial class StartControl : UserControl
+    {
+        int LeagueID = 0;
+        List<Team> LeagueTeams;
+        List<League> Leagues;
+        TeamAdapter ta = new TeamAdapter();
+
+        private class SimpleTeamListItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public StartControl()
+        {
+            InitializeComponent();
+
+            LeagueAdapter la = new LeagueAdapter();
+            Leagues = la.GetLeagues();
+
+            foreach(League l in Leagues)
+            {
+                LeaguePaging.Items.Add(new PagingItem(){ ID = l.UniqueID, Name = l.Name });
+            }
+
+            LeaguePaging.DisplayItem(0);
+
+            UpdateTeams();
+
+        }
+
+        private void UpdateTeams()
+        {
+            LeagueID = LeaguePaging.Items[LeaguePaging.CurrentItem].ID;
+            LeagueTeams = ta.GetTeamsByLeague(LeagueID);
+            lvwTeams.ItemsSource = LeagueTeams;
+        }
+
+        private void LeaguePaging_EitherDirectionClicked(object sender, EventArgs e)
+        {
+            UpdateTeams();
+        }
+    }
+}
---
+++ GoalLine.UI/SingleControls/PagingControl.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoalLine.UI.SingleControls
+{
+    /// <summary>
+    /// Interaction logic for PagingControl.xaml
+    /// </summary>
+    public partial class PagingControl : UserControl
+    {
+        public event EventHandler GoBackClicked;
+        public event EventHandler GoForwardClicked;
+        public event EventHandler EitherDirectionClicked;
+
+        private int _CurrentItem = -1;
+        public int CurrentItem
+        {
+            get
+            {
+                return _CurrentItem;
+            }
+
+            set
+            {
+                DisplayItem(value);
+            }
+        }
+        public List<PagingItem> Items = new List<PagingItem>();
+
+        public void DisplayItem(int Sequence)
+        {
+            _CurrentItem = Sequence;
+            lblCaption.Text = Items[CurrentItem].Name;
+        }
+
+        public PagingControl()
+        {
+            InitializeComponent();
+        }
+
+        private void GoForward_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _CurrentItem++;
+            if(CurrentItem >= Items.Count)
+            {
+                _CurrentItem = 0;
+            }
+
+            DisplayItem(CurrentItem);
+            GoForwardClicked?.Invoke(this, EventArgs.Empty);
+            EitherDirectionClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void GoBack_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _CurrentItem--;
+            if (CurrentItem < 0)
+            {
+                _CurrentItem = Items.Count - 1;
+            }
+
+            DisplayItem(CurrentItem);
+            GoBackClicked?.Invoke(this, EventArgs.Empty);
+            EitherDirectionClicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class PagingItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
